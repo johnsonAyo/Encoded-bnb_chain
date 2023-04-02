cb8b15a7ff35300de1965e5fe2c2a2970f2e7af0
ambiti0n <fabulousfresh010@gmail.com>
2023-04-02 10:48:48 +0100

    level2 challenge

---
+++ 4th_Module/Level2.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.8.0;
+
+interface Isolution2 {
+    function solution(uint256[10] calldata unsortedArray)
+        external
+        returns (uint256[10] memory sortedArray);
+}
+
+contract Solution2 {
+    function solution(uint256[10] calldata unsortedArray)
+        external
+        pure
+        returns (uint256[10] memory sortedArray)
+    {
+        // Copy the input array to a new array
+        uint256[10] memory arr = unsortedArray;
+
+        // Sort the array in ascending order using bubble sort
+        for (uint256 i = 0; i < arr.length - 1; i++) {
+            for (uint256 j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap the elements
+                    uint256 temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        // Return the sorted array
+        sortedArray = arr;
+    }
+}
--- 4th_Module/Readme.md
+++ 4th_Module/Readme.md
@@ -22,3 +22,22 @@ contract Level_0_Solution {
     }
 
 }
+
+Level 1 - Matrix Addition
+Write a function that adds two matrices returns the result. To keep things simple the array sizes will be fixed sizes of 2x2 (uint256[2][2]). Take a look at Wikipedia if you need help understanding matrix addition. Your solution should implement the following interface:
+
+interface Isolution1 {
+function solution(
+uint256[2][2] calldata x,
+uint256[2][2] calldata y
+) external pure returns (
+uint256[2][2] memory
+);
+}
+
+Level 2 - Sorting an Array
+Write a function that sorts an array in ascending order and returns the result. The array will be a fixed of 10 but the contents random. Your solution should implement the following interface:
+
+interface Isolution2 {
+function solution(uint256[10] calldata unsortedArray) external returns (uint256[10] memory sortedArray);
+}
